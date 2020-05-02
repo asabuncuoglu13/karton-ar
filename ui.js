f908c7e140ae905c6820f1575d8a72f8bcbdd199
asabuncuoglu13 <asabuncuoglu13@ku.edu.tr>
2020-05-02 22:11:52 +0300

    dimension-block-added

--- ui.js
+++ ui.js
@@ -7,6 +7,8 @@ function openUIDialog(id) {
         appendColorSlider();
     } else if (id === for_loop) {
         appendForLoopSlider();
+    } else if (id === dimension) {
+        appendDimensionSlider();
     } else {
     }
 }
@@ -26,10 +28,10 @@ function refreshSwatch() {
 function appendColorSlider() {
     $("#dialog").html(
         "<p id='red-slider-text'>Kırmızı oranını ayarlayın: </p>" +
-        "<div id='redSlider'></div>\n" +
-        "<div id='greenSlider'></div>\n" +
-        "<div id='blueSlider'></div>\n" +
-        "<div id='swatchArea' class='ui-widget-content ui-corner-all'></div>" +
+        "<div id='redSlider' class='pop-slider'></div>\n" +
+        "<div id='greenSlider' class='pop-slider'></div>\n" +
+        "<div id='blueSlider' class='pop-slider'></div>\n" +
+        "<div id='swatchArea' class='pop-result ui-widget-content ui-corner-all'></div>" +
         "<script>\n" +
         "$( '#redSlider, #greenSlider, #blueSlider' ).slider({\n" +
         "    orientation: 'horizontal',\n" +
@@ -46,17 +48,17 @@ function appendColorSlider() {
 }
 
 function refreshCount() {
-    loopTimes = $("#singleSlider").slider("value");
+    loopTimes = $("#countSlider").slider("value");
     $("#loopCount").html(loopTimes);
 }
 
 function appendForLoopSlider() {
     $("#dialog").html(
         "<p id='for-loop-slider-text'>Döngü miktarını belirleyin: </p>" +
-        "<div id='singleSlider'></div>\n" +
-        "<p id='loopCount' class='ui-widget-content ui-corner-all'></p>" +
+        "<div id='countSlider' class='pop-slider'></div>\n" +
+        "<p id='loopCount' class='pop-result ui-widget-content ui-corner-all'></p>" +
         "<script>\n" +
-        "$( '#singleSlider' ).slider({\n" +
+        "$( '#countSlider' ).slider({\n" +
         "    orientation: 'horizontal',\n" +
         "    range: 'min',\n" +
         "    max: 20,\n" +
@@ -68,5 +70,32 @@ function appendForLoopSlider() {
         "</script>\n");
 }
 
+function refreshDimension() {
+    w = $("#widthSlider").slider("value");
+    h = $("#heightSlider").slider("value");
+    d = $("#depthSlider").slider("value");
+    $("#dimensionArea").html(w + ", " + h + ", " + d);
+}
+
 function appendDimensionSlider() {
+    $("#dialog").html(
+        "<p id='red-slider-text'>Boyutları belirleyin: </p>" +
+        "<div id='widthSlider' class='pop-slider'></div>\n" +
+        "<div id='heightSlider' class='pop-slider'></div>\n" +
+        "<div id='depthSlider' class='pop-slider'></div>\n" +
+        "<div id='dimensionArea' class='pop-result ui-widget-content ui-corner-all'></div>" +
+        "<script>\n" +
+        "$( '#widthSlider, #heightSlider, #depthSlider' ).slider({\n" +
+        "    orientation: 'horizontal',\n" +
+        "    range: 'min',\n" +
+        "    max: 3,\n" +
+        "    value: 1,\n" +
+        "    step: 0.2, \n" +
+        "    slide: refreshDimension,\n" +
+        "    change: refreshDimension\n" +
+        "});\n" +
+        "$( '#widthSlider' ).slider( 'value', 1 );\n" +
+        "$( '#heightSlider' ).slider( 'value', 1 );\n" +
+        "$( '#depthSlider' ).slider( 'value', 1 );" +
+        "</script>\n");
 }
